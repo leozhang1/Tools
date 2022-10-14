2887bee722b94b76656c24a02686d47afbf55122
Leo Zhang <leotheasianlion@gmail.com>
2022-10-14 03:31:48 -0400

    update
    fetching data from database now working and fixed typo for the github automation query

--- RetrieveInfo.py
+++ RetrieveInfo.py
@@ -1,6 +1,7 @@
 import requests
 import pprint
 import json
+import os
 
 data = [
 	{"title": "okcupidbot"},
@@ -8,10 +9,9 @@ data = [
 	{"title": "bmbl_bot"},
 	{"title": "FlightScraper"},
 	{"title": "GeneralDB"},
-	{"title": "GitHubAutomation"},
+	{"title": "GithubAutomation"},
 	{"title": "NewsScraper"},
 	{"title": "TaxLienScraper"},
-	{"title": "Zoom-Automation-Python"},
 	{"title": "automate_texting"},
 	{"title": "leo_personal_website"},
 ]
@@ -19,19 +19,23 @@ data = [
 r = requests.get("http://localhost:3001/gh-router/get", json=data)
 
 res = json.loads((r.content).decode('utf-8'))
-pprint.pprint(res)
-print(type(res))
+#pprint.pprint(res)
+#print(type(res))
 
-#baseDir = '/home/leo_zhang/Documents/GitHub/'
-#currentGitHubFolder = None
-#for obj in res:
-#    for k, v in obj:
-#        if k == 'title':
-#            currentGitHubFolder = v
-#        elif k == 'secrets':
-#            # create file in the directory baseDir + v
-#            if currentGitHubFolder:
-#                with open(baseDir+currentGitHubFolder+'/.env', 'a') as f:
-#                    for exp in v:
-#                        f.write(exp)
-#
+baseDir = '/home/leo_zhang/Documents/GitHub/'
+currentGitHubFolder = None
+for obj in res:
+    for k, v in obj.items():
+        if k == 'title':
+            currentGitHubFolder = v
+        elif k == 'secrets':
+            # create file in the directory baseDir + v
+            if currentGitHubFolder:
+                print(f'adding .env with the contents below to file to {currentGitHubFolder}')
+                print(v)
+                if not os.path.exists(baseDir+currentGitHubFolder+'/.env'):
+                    open(baseDir+currentGitHubFolder+'/.env', 'w').close()
+                with open(baseDir+currentGitHubFolder+'/.env', 'w+') as f:
+                    for exp in v:
+                        f.write(exp+'\n')
+
