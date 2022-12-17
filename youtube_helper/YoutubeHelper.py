84f4bb170d38cabaf9aa5ab402eb3bdca4cd9e3e
Leo Zhang <leotheasianlion@gmail.com>
2022-12-16 20:14:38 -0500

    update

--- youtube_helper/YoutubeHelper.py
+++ youtube_helper/YoutubeHelper.py
@@ -3,42 +3,39 @@
 # Prerequistes mentioned in the video:
 # In settings:
 #   - make sure videos are never made for kids
+import json
 import os
 import pwd
+import random
+from time import sleep
 
 import undetected_chromedriver as uc
 from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from time import sleep
-
+from selenium.webdriver.common.by import By
 
 os.chdir(os.path.dirname(__file__))
 
-chrome_options = Options()
-# options.add_experimental_option('excludeSwitches', ['enable-logging'])
-chrome_options.add_argument("--log-level=3")
-chrome_options.set_capability("detach", True)
-# which chrome profile to use
-chrome_options.add_argument('--profile-directory=Default')
-# directory containing all the profiles
-chrome_options.add_argument(f"--user-data-dir=/home/{pwd.getpwuid(os.getuid()).pw_name}/.config/google-chrome/")
-chrome_options.binary_location = "/opt/google/chrome/google-chrome"
-print("\033[1;31;40m IMPORTANT: Put one or more videos in the *videos* folder in the bot directory. Please make sure to name the video files like this --> Ex: vid1.mp4 vid2.mp4 vid3.mp4 etc..")
-# sleep(6)
+videos_path = f'{os.getcwd()}/videos'
 
-print("\033[1;31;40m IMPORTANT: Please make sure the name of the videos are like this: vid1.mp4, vid2.mp4, vid3.mp4 ...  etc")
+configData = None
+with open(f"{os.getcwd()}/config.json") as configFile:
+    configData = json.load(configFile)
 
-dir_path = f'{os.getcwd()}/videos'
-count = 0
+keys = configData.keys()
 
-for path in os.listdir(dir_path):
-    if os.path.isfile(os.path.join(dir_path, path)):
-        count += 1
-print("   ", count, " Videos found in the videos folder, ready to upload...")
-sleep(6)
+videos = os.listdir(videos_path)
 
-for i in range(2,6):
+for key, video_file in zip(keys, videos):
+    chrome_options = Options()
+    # options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    chrome_options.add_argument("--log-level=3")
+    chrome_options.set_capability("detach", True)
+    # which chrome profile to use
+    chrome_options.add_argument('--profile-directory=Default')
+    # directory containing all the profiles
+    chrome_options.add_argument(f"--user-data-dir=/home/{pwd.getpwuid(os.getuid()).pw_name}/.config/google-chrome/")
+    chrome_options.binary_location = "/opt/google/chrome/google-chrome"
     bot = uc.Chrome(options=chrome_options)
     bot.get("https://studio.youtube.com")
     sleep(3)
@@ -47,22 +44,20 @@ for i in range(2,6):
     sleep(1)
 
     file_input = bot.find_element(By.XPATH, '//*[@id="content"]/input')
-    simp_path = f'videos/vid{i}.mkv'
-    abs_path = os.path.abspath(simp_path)
+    file_input.send_keys(os.path.join(videos_path, video_file))
 
-    file_input.send_keys(abs_path)
-
-    sleep(7)
+    sleep(random.uniform(7,10))
 
     inputs = bot.find_elements(By.XPATH, '//*[@id="textbox"]')
 
     title, desc = inputs
 
-    # TODO: grab from config file
     title.clear()
-    title.send_keys('title')
+    title.send_keys(configData[key]['title'])
     desc.clear()
-    desc.send_keys('desc')
+    desc.send_keys(configData[key]['description'])
+
+    sleep(random.uniform(5,7))
 
     next_button = bot.find_element(By.XPATH, '//*[@id="next-button"]')
     for i in range(3):
@@ -72,10 +67,10 @@ for i in range(2,6):
     done_button = bot.find_element(By.XPATH, '//*[@id="done-button"]')
     done_button.click()
     sleep(5)
-    bot.quit()
+    bot.close()
 
     print('finished with this video')
-    sleep(5000)
+    # sleep(5000)
 
 
 
